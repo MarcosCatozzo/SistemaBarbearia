179c0b3108ed51d25b4ecfbf9c5fdcb011f8d404
marco <marcos.catozzo2000@gmail.com>
2025-06-06 14:52:14 -0300

    Refatoracao de pacote

--- src/main/java/barbearia/api/infra/security/SecurityConfiguration.java
+++ src/main/java/barbearia/api/infra/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package barbearia.api.infra;
+package barbearia.api.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
