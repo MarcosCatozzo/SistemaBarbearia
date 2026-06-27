46c1c1c962ed1026a28c9779952da71d69eb94f0
marco <marcos.catozzo2000@gmail.com>
2026-06-26 23:58:10 -0300

    exclusao do smtp para teste em ambiente de prod e incremento de versão

--- src/main/java/barbearia/api/domain/service/EmailService.java
+++ src/main/java/barbearia/api/domain/service/EmailService.java
@@ -1,41 +1,41 @@
-package barbearia.api.domain.service;
-
-import barbearia.api.domain.dto.Email;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-
-@Service
-public class EmailService {
-	private final JavaMailSender mailSender;
-	private final TemplateEngine templateEngine;
-
-	public EmailService(JavaMailSender mailSender, TemplateEngine templateEngine) {
-		this.mailSender = mailSender;
-		this.templateEngine = templateEngine;
-	}
-
-	public void envioDeEmail(Email email) throws MessagingException{
-
-		String linkConfirmacao = "http://localhost:8080/confirmar=true";
-
-		Context context = new Context();
-		context.setVariable("linkConfirmacao", linkConfirmacao);
-		
-		String htmlContent = templateEngine.process("confirmacao-email", context);
-		
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(message, true,"UTF-8");
-		
-		helper.setFrom("barberCortes01@gmail.com"); 		// remetente
-		helper.setTo(email.to());                    		// destinatário
-		helper.setSubject(email.subject());           		// assunto
-		helper.setText(htmlContent, true);
-
-		mailSender.send(message);
-	}
-}
+//package barbearia.api.domain.service;
+//
+//import barbearia.api.domain.dto.Email;
+//import jakarta.mail.MessagingException;
+//import jakarta.mail.internet.MimeMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.stereotype.Service;
+//import org.thymeleaf.TemplateEngine;
+//import org.thymeleaf.context.Context;
+//
+//@Service
+//public class EmailService {
+//	private final JavaMailSender mailSender;
+//	private final TemplateEngine templateEngine;
+//
+//	public EmailService(JavaMailSender mailSender, TemplateEngine templateEngine) {
+//		this.mailSender = mailSender;
+//		this.templateEngine = templateEngine;
+//	}
+//
+//	public void envioDeEmail(Email email) throws MessagingException{
+//
+//		String linkConfirmacao = "http://localhost:8080/confirmar=true";
+//
+//		Context context = new Context();
+//		context.setVariable("linkConfirmacao", linkConfirmacao);
+//
+//		String htmlContent = templateEngine.process("confirmacao-email", context);
+//
+//		MimeMessage message = mailSender.createMimeMessage();
+//		MimeMessageHelper helper = new MimeMessageHelper(message, true,"UTF-8");
+//
+//		helper.setFrom("barberCortes01@gmail.com"); 		// remetente
+//		helper.setTo(email.to());                    		// destinatário
+//		helper.setSubject(email.subject());           		// assunto
+//		helper.setText(htmlContent, true);
+//
+//		mailSender.send(message);
+//	}
+//}
